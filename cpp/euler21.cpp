3be0b591340f1c8307d0fb271731a86608e56ad9
collinshayden <48135191+collinshayden@users.noreply.github.com>
2020-06-09 20:27:12 -0400

    Create euler21.cpp

---
+++ cpp/euler21.cpp
@@ -0,0 +1,40 @@
+// Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
+// If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+// For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. 
+// The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+// Evaluate the sum of all the amicable numbers under 10000.
+#include <iostream>
+using namespace std; 
+
+int factors(int n)
+{
+  int sum = 0; int step;
+  if (n % 2==0) {//twice as efficient for even numbers
+    step = 2; sum = 1;//sum = 1 because 1 is otherwise skipped for even numbers
+  } else step = 1;//if odd, step has to be 1
+
+  for (int i = step; i < (n/2) + 1; i++){//for normal factors, upper bound is n/2
+    if (n % i==0){
+      sum += i;
+    }
+  } return sum;
+}
+
+int totalsum()
+{
+  int totalsum = 0; int perfectnums = 1+6+28+496+8128;
+  for (int n = 0; n < 10000; n++){
+    if (factors(factors(n)) == n){
+      totalsum += n;
+      cout<<n<<endl;
+    }
+  } return totalsum-perfectnums;//question asks for only amicable numbers, excluding perfect numbers(sum of divisers = n)
+}
+
+int main()
+{
+  cout<<totalsum()<<endl;
+}
+
+//output is 31626
+//time 0m0.359s
